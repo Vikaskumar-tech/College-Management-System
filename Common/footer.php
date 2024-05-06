d1bd63d6d9214d7694377dca221c4b1a6b7e4ec9
Vikas Kumar <113982212+Vikaskumar-tech@users.noreply.github.com>
2024-05-06 17:54:45 +0530

    Add files via upload

---
+++ Common/footer.php
@@ -0,0 +1,43 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-one md-mb-30 sm-mb-30">
+            <h3>Riet College</h3>
+            <p>This Project has been made by Vikas Kumar with the help of 
+              my team .
+              The prupose of this project is Learning Computer Languages.
+              I hope you realy enjoyed to visit this project. Thanks!
+            </p>
+          </div>
+          <div class="col-md-2 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>Quick Links</h2>
+            <ul>
+              <li><a href="">About RIET-Jaipur</a></li>
+              <li><a href="">Admissions Policy</a></li>
+              <li><a href="">Apply for Admission</a></li>
+              <li><a href="">Download Prospectus</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.tech Civil Engineering</a></li>
+              <li><a href="">B.tech Electrical Engineering</a></li>
+              <li><a href="">B.tech Mechanical Engineering</a></li>
+              <li><a href="">B.tech Computer Science & Engineering</a></li>
+              <li><a href="">B.tech AI&DS</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address: Rajasthan Institute of Engineering and Technology, Bhankrota, Ajmer Road, Jaipur</p>
+              <p>Telephones: +91-141-2250066, 2251193</p>
+              <p>E-mail: admin@rietjaipur.ac.in</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text">CopyRights 2023 © reserved by Skillwide </p>
+</footer>
